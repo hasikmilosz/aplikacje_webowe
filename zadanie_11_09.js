634d6c5541e343c07ca3ac52b8d7825bc5eebe6a
mhaski <145277635+hasikmilosz@users.noreply.github.com>
2025-09-11 07:55:08 +0200

    Add files via upload

---
+++ zadanie_11_09.js
@@ -0,0 +1,24 @@
+let x = 67
+const tab = ["six","seven","eight"]
+let a = 41
+function add(a,b){
+    console.log(a+b)
+}
+function sixSeven(number){
+    if(number = 67){
+        console.log("six seven")
+    }
+}
+function evenNumbers(count){
+    let b=0
+    for(let i=0;i<count;i++){
+        console.log(b+" ")
+        b+=2
+    }
+}
+add(a,x)
+console.log("\n")
+sixSeven(x)
+console.log("\n")
+evenNumbers(10)
+console.log("\n")
